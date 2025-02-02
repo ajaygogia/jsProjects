e02917c108613605857766405b799be2d29ffdf5
ajaygogia <ajaygogia26@gmail.com>
2025-02-03 01:39:01 +0530

    Add files via upload

---
+++ PROJECTS/PROGRESS BAR/practice.js
@@ -0,0 +1,38 @@
+let progress = document.getElementById('progress');
+let timer = 10;
+let width = 0;
+let interval
+
+function start() {
+    document.getElementById('start').disabled = true;
+    document.getElementById('range').disabled = true;
+    progress.style.width = width + '%';
+    let increase = 100 / (timer * 100);
+    interval = setInterval(() => {
+        if (width >= 100) {
+            clearInterval(interval);
+        } else {
+            width = width + increase;
+            progress.style.width = width + '%';
+        }
+    }, 10);
+}
+
+function pause() {
+    clearInterval(interval);
+    document.getElementById('start').disabled = false;
+}
+
+function reset() {
+    width = 0;
+    progress.style.width = width + '%';
+    clearInterval(interval);
+    document.getElementById('start').disabled = false;
+    document.getElementById('range').disabled = false;
+
+}
+
+function updateValue(value) {
+    document.getElementById('range-value').innerText = value;
+    timer = value;
+}
